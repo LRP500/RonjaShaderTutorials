9a3a65a22194b91f322d9cf20edd29c76c5caa72
pierre.roy <pierre.roy@epitech.eu>
2019-12-23 00:27:01 +0100

    15_PostProcessingBasics

---
+++ Assets/15_PostProcessingBasics/PostProcessing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostProcessing : MonoBehaviour
+{
+    [SerializeField]
+    private Material _postProcessMaterial = null;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _postProcessMaterial);
+    }
+}
