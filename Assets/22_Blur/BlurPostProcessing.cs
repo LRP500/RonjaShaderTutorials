8692f54d85394d8278607e52c95f1356a801449a
pierre.roy <pierre.roy@epitech.eu>
2020-01-03 17:41:52 +0100

    22_Blur

---
+++ Assets/22_Blur/BlurPostProcessing.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BlurPostProcessing : MonoBehaviour
+{
+    [SerializeField]
+    private Material _postProcessingMaterial = null;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        RenderTexture temporaryTexture = RenderTexture.GetTemporary(source.width, source.height);
+        Graphics.Blit(source, temporaryTexture, _postProcessingMaterial, 0);
+        Graphics.Blit(temporaryTexture, destination, _postProcessingMaterial, 1);
+        RenderTexture.ReleaseTemporary(temporaryTexture);
+    }
+}
